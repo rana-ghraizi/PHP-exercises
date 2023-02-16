ef604d5abf3129cb8499e65b9aaf7a9fbe90fbee
Rana Ghraizi <ranaghraizi1@gmail.com>
2023-02-16 20:34:16 +0200

    Step_3

---
+++ php-basics-01/Step_3.php
@@ -0,0 +1,11 @@
+<?php
+$name = "Rana";
+$email = "ranaghraizi1@gmail.com";
+$height = "10";
+$width = "8";
+$area = $height * $width;
+
+// Output
+echo "My name is $name and my email is $email\n";
+echo "the area is $area";
+?>
