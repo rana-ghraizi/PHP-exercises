66b743849fd95ca141adf11b2e9effdc67c0126f
Rana Ghraizi <ranaghraizi1@gmail.com>
2023-02-17 20:01:20 +0200

    Step_8-b

---
+++ php-basics-01/Step_8-b.php
@@ -0,0 +1,8 @@
+<?php
+$grocery = array (
+    array ('eggs', 'balade', 'mazere3'),
+    array ('milk', 'Fresh', 'Taanayel'),
+    array ('water-pack', 'Tanoureen', 'Reem')
+);
+echo "Hey Sir, Please I need 1 pack of " . $grocery[0][1] . " ". $grocery[0][0] ." and 3 " . $grocery[2][2] . " ". $grocery[2][0] . ".";
+?>
